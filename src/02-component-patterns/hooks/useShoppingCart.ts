54001864618ab3cdaa9c5784003ff0aab35f3838
Joseph98Leo <joseph98.gutierrez@gmail.com>
2023-03-13 18:58:25 -0500

    Control props alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -12,32 +12,17 @@ export const useShoppingCart = () => {
 
         setShoppingCart(oldShoppingCart => {
 
-            const productInCard: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-            if( Math.max( productInCard.count + count, 0 ) > 0){
-                productInCard.count += count;
-                return {
-                    ...oldShoppingCart,
-                    [ product.id ]: productInCard
-                }
-            }
-
-            // BORRAR EL PRODUCTO
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return {...rest};
-
-        //     if( count === 0 ){
-        //         const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-        //         return rest;
+            if( count === 0 ){
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                return rest;
                 // delete oldShoppingCart[ product.id ]
                 // return { ...oldShoppingCart }
-        //     }
+            }
 
-        //     return {
-        //         ...oldShoppingCart,
-        //         [ product.id ]: { ...product, count }
-        //     }
-        // })
+            return {
+                ...oldShoppingCart,
+                [ product.id ]: { ...product, count }
+            }
         })
     }
 
